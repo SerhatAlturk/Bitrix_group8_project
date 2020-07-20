3f7516d10989466a9af0c3db085cc56299f9a324
Altur <alturkserht@gmail.com>
2020-07-20 11:02:48 -0400

    Added Hooks class to the step_definitions
    Added Cucumber run commands to MainRunner class

---
+++ src/test/java/com/Bitrix24/runner/MainRunner.java
@@ -0,0 +1,25 @@
+package com.Bitrix24.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions
+		(
+				plugin =
+						{
+								"html:target/cucumber-report.html",
+								"json:target/cucumber.json",
+								"rerun:target/rerun.txt"
+						},
+				features = "features",
+				glue = "com/Bitrix24/step_definitions",
+				dryRun = false,
+				tags = ""
+		
+		)
+
+public class MainRunner
+{
+}
